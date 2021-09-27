772536dcbb0e5a9adf5197332fe6bd1ca8891f24
tuyen.nguyen <tuyen.nguyen@savvycomsoftware.com>
2021-09-27 08:48:29 +0700

    Update Expandable text widget

--- lib/src/widgets/expandable_rich_text.dart
+++ lib/src/widgets/expandable_rich_text.dart
@@ -1,12 +1,10 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class ExpandableRichText extends StatefulWidget {
   const ExpandableRichText(
-      this.text, {
-        this.maxLines = 5,
-      }) : assert(text != null),
+    this.text, {
+    this.maxLines = 5,
+  })  : assert(text != null),
         assert(maxLines != null && maxLines > 0);
 
   final String text;
@@ -27,42 +25,56 @@ class ExpandableRichTextState extends State<ExpandableRichText> {
 
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(builder: (context, sizeLayout) {
-      TextSpan span =  TextSpan(
-        text: """${widget.text}""",
-        style: TextStyle(
+    return LayoutBuilder(
+      builder: (context, sizeLayout) {
+        final TextSpan span = TextSpan(
+          text: widget.text,
+          style: const TextStyle(
             color: Colors.red,
             fontSize: 20,
-            fontWeight: FontWeight.normal
-        ),
-      );
-      final tp = TextPainter(text: span, maxLines: maxLines, textDirection:TextDirection.ltr );
-      tp.layout(maxWidth: sizeLayout.maxWidth);
-      print(tp.didExceedMaxLines);
-      return Container(
-        // width: 200,
-        child: Column(
-          children: [
-            RichText(
-              text: span,
-              maxLines: maxLines,
-            ),
-            Visibility(
-              visible: tp.didExceedMaxLines,
-              child: InkWell(
-                onTap: () => setState(() => maxLines = 1000),
-                child: Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 10),
-                    child: Icon(Icons.keyboard_arrow_down_outlined, color: Colors.blue,),
+            fontWeight: FontWeight.normal,
+          ),
+        );
+        final TextPainter tp = TextPainter(
+          text: span,
+          maxLines: maxLines,
+          textDirection: TextDirection.ltr,
+        );
+        tp.layout(maxWidth: sizeLayout.maxWidth);
+        print(tp.didExceedMaxLines);
+        return IntrinsicHeight(
+          child: Row(
+            children: [
+              Expanded(
+                child: RichText(
+                  text: span,
+                  maxLines: maxLines,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Visibility(
+                visible: tp.didExceedMaxLines,
+                child: InkWell(
+                  onTap: () => setState(() => maxLines = 1000),
+                  child: Container(
+                    height: double.infinity,
+                    alignment: Alignment.bottomRight,
+                    child: Row(
+                      children: const [
+                        Text('Show'),
+                        Icon(
+                          Icons.keyboard_arrow_down_outlined,
+                          color: Colors.blue,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
-      );
-    
-    });
+            ],
+          ),
+        );
+      },
+    );
   }
 }
