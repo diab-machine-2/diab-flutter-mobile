51d25d9209972531c1e38f4ba04536cd084f7a81
Tuyen Nguyen <tuyen.nguyen@savvycomsoftware@gmail.com>
2021-12-10 14:32:05 +0700

    Revert https to http

--- lib/src/repo/food/food_client.dart
+++ lib/src/repo/food/food_client.dart
@@ -231,7 +231,7 @@ class FoodClient extends FetchClient {
         params['foods[$i].portion'] = foods[i].portion.toString();
       }
       final response = await super
-          .postHttps(path: '/App/Diet/Input', params: params, files: files);
+          .postHttp(path: '/App/Diet/Input', params: params, files: files);
       if (response.statusCode == 200) {
         return true;
       } else {
