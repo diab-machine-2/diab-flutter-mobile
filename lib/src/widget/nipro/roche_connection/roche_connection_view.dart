6003c3b270fc93901c4c947a2c559906ab0a060b
thiemphu413 <thienphu413@gmail.com>
2023-06-06 10:34:20 +0700

    Update

---
+++ lib/src/widget/nipro/roche_connection/roche_connection_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:medical/src/widgets/normal_template.dart';
+
+class RocheConnectionView extends StatefulWidget {
+  const RocheConnectionView({Key? key}) : super(key: key);
+
+  @override
+  State<RocheConnectionView> createState() => _RocheConnectionViewState();
+}
+
+class _RocheConnectionViewState extends State<RocheConnectionView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NormalTemplate(
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
