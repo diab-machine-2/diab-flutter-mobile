77e03c3712d8f0d6302b976f1b72be1e2ed95c09
Phuc Tran <ngocphuc1612@gmail.com>
2024-02-21 17:42:06 +0700

    change how to trigger zoom

--- lib/src/app_setting/firebase_remote_config.dart
+++ lib/src/app_setting/firebase_remote_config.dart
@@ -11,12 +11,14 @@ class FirebaseRemoteSetting {
   late String _appStoreVersion;
   late String _playStoreVersion;
   String? _storeNavigationUrl;
+  bool? _appDeveloperMode = false;
 
   String get appStoreVersion => _appStoreVersion;
   String get playStoreVersion => _playStoreVersion;
   String get storeNavigationUrl {
     return _storeNavigationUrl ?? 'https://diab.com.vn/ve-diab/';
   }
+  bool get appDeveloperMode => _appDeveloperMode ?? false;
 
   Future<void> init() async {
     await remoteConfig.setConfigSettings(RemoteConfigSettings(
@@ -27,5 +29,6 @@ class FirebaseRemoteSetting {
     _appStoreVersion = remoteConfig.getString('APP_STORE_VERSION');
     _playStoreVersion = remoteConfig.getString('PLAY_STORE_VERSION');
     _storeNavigationUrl = remoteConfig.getString('STORE_NAVIGATION_URL');
+    _appDeveloperMode = remoteConfig.getBool('APP_DEVELOPER_MODE');
   }
 }
