89eaf19f6bb76d689b8f8600deaee99ee802bc6e
phuctran <ngocphuc1612@gmail.com>
2025-05-16 17:03:43 +0700

    fix BP-27, BP-31

--- lib/src/widget/tabbar/tabbar_v2.dart
+++ lib/src/widget/tabbar/tabbar_v2.dart
@@ -178,12 +178,16 @@ class _TabbarControllerState extends State<TabbarController> with Observer {
     } else if (index == TabBarType.chat.index) {
       // _jumpTo(index);
       // _lastIndex = index;
-      Navigator.pushNamed(context, NavigatorName.conversation_chatbot_ai);
+      _onChatWithAI();
     } else if (index == -1) {
       // _showMaterialDialog();
     } else {
       _jumpTo(index);
     }
+  }
+
+  void _onChatWithAI() {
+    Navigator.pushNamed(context, NavigatorName.conversation_chatbot_ai);
   }
 
   void _checkExistZoomId() async {
@@ -270,7 +274,7 @@ class _TabbarControllerState extends State<TabbarController> with Observer {
     }
     if (notifyName == Const.NAVIGATE_TO_CHAT_TAB) {
       Navigator.of(context).popUntil((route) => route.isFirst || route.settings.name == NavigatorName.tabbar);
-      _jumpTo(TabBarType.chat.index);
+      _onChatWithAI();
     }
     if (notifyName == Const.NAVIGATE_TO_LESSON_DETAIL ||
         notifyName == Const.NAVIGATE_TO_ACTIVITY_DETAIL) {
