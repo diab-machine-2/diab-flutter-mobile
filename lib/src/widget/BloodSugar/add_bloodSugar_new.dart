bf84eb4658703b1ce801f2b50afbf6292bef6abd
Tri Nguyen <tri.nguyen@diab.com.vn>
2025-08-13 18:00:26 +0700

    prevent trigger mark complete smart goal when input manual not go through activites

--- lib/src/widget/BloodSugar/add_bloodSugar_new.dart
+++ lib/src/widget/BloodSugar/add_bloodSugar_new.dart
@@ -656,8 +656,10 @@ class _AddBloodSugarControllerNewState
             'method': fromNipro ? 'device' : 'manual',
           },
         );
-        await HomeClient().completeSmartGoal(selectedDate, widget.goalId ?? '',
-            1, ScheduleType.blood_sugar.typeIndex);
+        if (widget.goalId != null && widget.goalId?.isNotEmpty == true) {
+          await HomeClient().completeSmartGoal(selectedDate,
+              widget.goalId ?? '', 1, ScheduleType.blood_sugar.typeIndex);
+        }
         _navigateAfterSuccess(result!.id, result.images);
       }
 
