8e5c9e9c8bd33d6560df08a4d86e2c1fea98bd0f
Tri Nguyen <tri.nguyen@diab.com.vn>
2025-09-15 15:32:16 +0700

    improve hide welcome package page

--- lib/src/widget/home/welcome_package_screen/welcome_package_screen_page.dart
+++ lib/src/widget/home/welcome_package_screen/welcome_package_screen_page.dart
@@ -401,6 +401,7 @@ class _WelcomePackageScreenPageState extends State<WelcomePackageScreenPage> {
   }
 
   Future<void> _handleButtonBookingConsultPress(String url) async {
+    isClickSkip = false;
     Navigator.pop(context);
     if (await canLaunch(url)) {
       FlutterBranchSdk.handleDeepLink(url);
@@ -427,19 +428,17 @@ class _WelcomePackageScreenPageState extends State<WelcomePackageScreenPage> {
 
   Future<void> _openZaloGroup(String zaloGroup) async {
     // Always mark welcome as displayed when user clicks join_zalo_group button
-    await _cubit.markDisplayedWelcome();
-    
+    if (!isClickSkip) {
+      isClickSkip = true;
+      await _backPressed();
+    }
+
     final url = Uri.tryParse(zaloGroup);
     if (url != null && await canLaunchUrl(url)) {
       await launchUrl(url);
     } else {
       // Handle case where URL cannot be launched
       print('[ONBOARDING] Could not launch Zalo group URL: $zaloGroup');
-    }
-    
-    if (!isClickSkip) {
-      isClickSkip = true;
-      await _backPressed();
     }
   }
 
