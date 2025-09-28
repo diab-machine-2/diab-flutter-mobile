9471f04339eb1f12b4c1d938435eed657481ee57
LAPTOP-JMVMS1FB\PC <phat1000vtc@gmail.com>
2025-09-28 21:21:52 +0700

    Revert "Update"
    This reverts commit 96460320946bf41f184769d9533d866e19e31d0c.

---
+++ lib/src/widget/Bmi/views/add_bmi_view_old/widgets/add_bmi_mixin.dart
@@ -0,0 +1,365 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:medical/res/R.dart';
+import 'package:medical/src/app_setting/app_setting.dart';
+
+import '../add_bmi_cubit.dart';
+
+mixin AddBmiMixin on Widget {
+  int findIndexInRanges({
+    required AddBmiCubit cubit,
+  }) {
+    List<double> ranges = cubit.rangeValue;
+    if (cubit.selectedWeight == null) return -1;
+
+    for (int i = 0; i < ranges.length - 1; i++) {
+      if (cubit.selectedWeight >= ranges[i] &&
+          cubit.selectedWeight <= ranges[i + 1]) {
+        return i;
+      }
+    }
+    return ranges.length - 1;
+  }
+
+  showDialogWarning(
+    BuildContext context, {
+    required AddBmiCubit cubit,
+  }) {
+    int indexRange = findIndexInRanges(cubit: cubit);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Container(
+          child: AlertDialog(
+              contentPadding: EdgeInsets.all(0),
+              content: Stack(children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(R.drawable.ic_warning, width: 64, height: 64),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                            R.string.weight_at.tr() +
+                                ' ${cubit.weightRanges != null ? cubit.weightRanges![indexRange].name : ""}',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: R.color.textDark,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(
+                            //'Cân nặng đang không ở mức lý tưởng. Mẹ cần chú ý chế độ dinh dưỡng và vận động phù hợp hơn.',
+                            AppSettings.userInfo!.diabetesName ==
+                                    "Đái tháo đường thai kỳ"
+                                ? R.string.weight_not_at_ideal_pregnant.tr()
+                                : R.string.weight_not_at_ideal.tr(),
+                            textAlign: TextAlign.center,
+                            style: R.style.normalTextStyle),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                  onTap: () {
+                                    cubit.controllerWeight.selection =
+                                        TextSelection(
+                                            baseOffset: 0,
+                                            extentOffset: cubit
+                                                .controllerWeight.text.length);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                      height: 43,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(200),
+                                          color: R.color.grayBorder),
+                                      child: Center(
+                                        child: Text(R.string.re_type.tr(),
+                                            style: TextStyle(
+                                                color: R.color.textDark,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600)),
+                                      ))),
+                            ),
+                            SizedBox(width: 14),
+                            Expanded(
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    Navigator.pop(context);
+                                    cubit.submitData();
+                                  },
+                                  child: Container(
+                                    height: 43,
+                                    decoration: BoxDecoration(
+                                        color: R.color.red,
+                                        borderRadius:
+                                            BorderRadius.circular(200),
+                                        gradient: LinearGradient(
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.centerRight,
+                                            colors: [
+                                              R.color.greenGradientTop,
+                                              R.color.greenGradientBottom
+                                            ])),
+                                    child: Center(
+                                      child: Text(R.string.i_understand.tr(),
+                                          style: TextStyle(
+                                              color: R.color.white,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600)),
+                                    ),
+                                  )),
+                            ),
+                          ])
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                      icon: Icon(Icons.close, color: R.color.color0xffBEC0C8),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                )
+              ])),
+        );
+      },
+    );
+  }
+
+  showDialogDelete(BuildContext context, AddBmiCubit cubit) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Container(
+          child: AlertDialog(
+              contentPadding: EdgeInsets.all(0),
+              content: Stack(children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(R.drawable.ic_earse, width: 64, height: 64),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(R.string.ban_muon_xoa_du_lieu.tr(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: R.color.textDark,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(R.string.confirm_to_remove_data.tr(),
+                            textAlign: TextAlign.center,
+                            style: R.style.normalTextStyle),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 16),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                      height: 43,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(200),
+                                          color: R.color.grayBorder),
+                                      child: Center(
+                                        child: Text(R.string.back.tr(),
+                                            style: TextStyle(
+                                                color: R.color.textDark,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600)),
+                                      )),
+                                ),
+                              ),
+                              SizedBox(width: 14),
+                              Expanded(
+                                child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    cubit.deleteData();
+                                  },
+                                  child: Container(
+                                    height: 43,
+                                    decoration: BoxDecoration(
+                                      color: R.color.red,
+                                      borderRadius: BorderRadius.circular(200),
+                                    ),
+                                    child: Center(
+                                      child: Text(R.string.delete.tr(),
+                                          style: TextStyle(
+                                              color: R.color.white,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600)),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                      icon: Icon(Icons.close, color: R.color.color0xffBEC0C8),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                )
+              ])),
+        );
+      },
+    );
+  }
+
+  showDialogSave(
+    BuildContext context, {
+    required AddBmiCubit cubit,
+  }) {
+    final note = cubit.controllerNote.text;
+    if (cubit.model != null) {
+      final date =
+          DateTime.fromMillisecondsSinceEpoch(cubit.model!.date! * 1000);
+      if (cubit.selectedWeight == 0 &&
+          cubit.selectedHeight == 0 &&
+          cubit.selectedHip == 0 &&
+          note.isEmpty &&
+          cubit.files.length == cubit.model!.images.length &&
+          cubit.removeIDs.length == 0 &&
+          date.millisecondsSinceEpoch ==
+              cubit.selectedDate.millisecondsSinceEpoch) {
+        Navigator.pop(context);
+        return;
+      }
+    } else if (cubit.selectedWeight == 0 &&
+        cubit.selectedHip == 0 &&
+        note.isEmpty &&
+        note.isEmpty &&
+        cubit.files.length == 0) {
+      Navigator.pop(context);
+      return;
+    }
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Container(
+          child: AlertDialog(
+              contentPadding: EdgeInsets.all(0),
+              content: Stack(children: [
+                Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(R.drawable.ic_back_icon,
+                          width: 64, height: 64),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(R.string.ban_muon_quay_lai.tr(),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                color: R.color.textDark,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text(R.string.confirm_to_back.tr(),
+                            textAlign: TextAlign.center,
+                            style: R.style.normalTextStyle),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                      height: 43,
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(200),
+                                          color: R.color.grayBorder),
+                                      child: Center(
+                                        child: Text(R.string.van_o_lai.tr(),
+                                            style: TextStyle(
+                                                color: R.color.textDark,
+                                                fontSize: 16,
+                                                fontWeight: FontWeight.w600)),
+                                      ))),
+                            ),
+                            SizedBox(width: 14),
+                            Expanded(
+                              child: GestureDetector(
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    Navigator.pop(context);
+                                  },
+                                  child: Container(
+                                    height: 43,
+                                    decoration: BoxDecoration(
+                                        color: R.color.red,
+                                        borderRadius:
+                                            BorderRadius.circular(200),
+                                        gradient: LinearGradient(
+                                            begin: Alignment.topLeft,
+                                            end: Alignment.centerRight,
+                                            colors: [
+                                              R.color.greenGradientTop,
+                                              R.color.greenGradientBottom
+                                            ])),
+                                    child: Center(
+                                      child: Text(R.string.exit.tr(),
+                                          style: TextStyle(
+                                              color: R.color.white,
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w600)),
+                                    ),
+                                  )),
+                            ),
+                          ])
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: IconButton(
+                      icon: Icon(Icons.close, color: R.color.color0xffBEC0C8),
+                      onPressed: () {
+                        Navigator.pop(context);
+                      }),
+                )
+              ])),
+        );
+      },
+    );
+  }
+}
