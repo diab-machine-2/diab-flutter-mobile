024788eb6cfec924e31877504f2d9c8c0ef637d9
LAPTOP-JMVMS1FB\PC <phat1000vtc@gmail.com>
2025-10-05 09:27:46 +0700

    Fix: issue 3

--- lib/src/widget/Bmi/views/add_bmi/add_bmi_page.dart
+++ lib/src/widget/Bmi/views/add_bmi/add_bmi_page.dart
@@ -64,33 +64,40 @@ class _AddBmiPageState extends State<AddBmiPage> {
       appBar: const AddBmiAppBar(),
       body: BlocListener<BmiInputBloc, BmiInputState>(
         listener: _handleListener,
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const AddBmiWeightInputSession(),
-              const SizedBox(
-                height: 12,
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const AddBmiWeightInputSession(),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const AddBmiWaistCircumferenceInputSession(),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const AddBmiNoteSession(),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    _Seperator(),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    _ConnectToHealthConnectButton()
+                  ],
+                ),
               ),
-              const AddBmiWaistCircumferenceInputSession(),
-              const SizedBox(
-                height: 12,
-              ),
-              const AddBmiNoteSession(),
-              const SizedBox(
-                height: 12,
-              ),
-              _Seperator(),
-              const SizedBox(
-                height: 12,
-              ),
-              _ConnectToHealthConnectButton()
-            ],
-          ),
+            ),
+            const _SaveButton()
+          ],
         ),
       ),
-      bottomNavigationBar: _SaveButton(),
     );
   }
 
